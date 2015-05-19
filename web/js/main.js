757ff07ae43b53855e4bdc8946a2b671e0ce5bbd
Guilherme <setetres@gmail.com>
2015-05-18 22:11:41 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     'use strict';
     $('.fit-video').fitVids();
+    $('.tile').css({height: window.innerHeight});
     $(window).on('debouncedresize',function() {
-        
+        $('.tile').css({height: window.innerHeight});
     });
 });
