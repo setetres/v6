c7c6713ea8e66ff3fb5fbd6ada452611c14f336e
Guilherme <setetres@gmail.com>
2015-05-27 22:11:54 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
         scrollingSpeed: 600,
         easingcss3: 'cubic-bezier(0.19, 1, 0.22, 1)',
         anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'cacete'],
-        menu: '#menu',
+        navigation: true,
+		navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH', 'Cacete Company'],
         onSlideLeave: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].stop();
         },
