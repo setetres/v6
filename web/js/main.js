267aaeb7bcca22f24ad35c8fd4cc1a83ab5153d2
Guilherme Pangnotta <setetres@gmail.com>
2015-05-28 16:10:14 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
         css3: true,
         scrollingSpeed: 600,
         easingcss3: 'cubic-bezier(0.19, 1, 0.22, 1)',
-        anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'puta', 'cacete', 'setetres'],
+        anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'puta', 'cacete', 'founders', 'fotoum', 'inkonik', 'setetres'],
         navigation: true,
-        navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'PUTA.nu', 'Cacete.co', 'Older versions'],
+        navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'PUTA.nu', 'Cacete.co', 'Founders.com.br', 'Fotoum.com', 'Inkonik.com', 'Older versions'],
         onSlideLeave: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].stop();
         },
