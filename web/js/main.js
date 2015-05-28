398ea8c98b28bc3a78e3904748c72d6ca05fae88
Guilherme Pangnotta <setetres@gmail.com>
2015-05-28 16:01:20 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,6 @@
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
 /* global objectFit */
+
 $(document).ready(function() {
     'use strict';
 
@@ -7,9 +8,9 @@ $(document).ready(function() {
         css3: true,
         scrollingSpeed: 600,
         easingcss3: 'cubic-bezier(0.19, 1, 0.22, 1)',
-        anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'cacete','xxxxxxxooo'],
+        anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'puta', 'cacete', 'setetres'],
         navigation: true,
-        navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'Cacete.co', 'XXXXXXX.OOO'],
+        navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'PUTA.nu', 'Cacete.co', 'Older versions'],
         onSlideLeave: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].stop();
         },
