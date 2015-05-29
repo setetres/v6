85b45b451da3ee2ca7bbcf23e841f6ede56bf9c7
Guilherme Pangnotta <setetres@gmail.com>
2015-05-29 10:05:49 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -8,15 +8,23 @@ $(document).ready(function() {
         css3: true,
         scrollingSpeed: 600,
         easingcss3: 'cubic-bezier(0.19, 1, 0.22, 1)',
-        anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'puta', 'cacete', 'founders', 'fotoum', 'inkonik', 'setetres'],
+        anchors: ['home', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'puta', 'cacete', 'founders', 'inkonik', 'setetres'],
         navigation: true,
-        navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'PUTA.nu', 'Cacete.co', 'Founders.com.br', 'Fotoum.com', 'Inkonik.com', 'Older versions'],
+        navigationTooltips: ['About', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'PUTA.nu', 'Cacete.co', 'Founders.com.br', 'Inkonik.com', 'Older versions'],
         onSlideLeave: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].stop();
         },
         afterLoad: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].play();
         }
+    });
+
+    // load
+
+    $(window).on('load',function(){
+        setTimeout(function(){
+            $('#loading').removeClass('active');
+        },1000);
     });
 
     // resize
