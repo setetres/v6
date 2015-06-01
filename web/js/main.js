aa7323d95d0bc6e90df92031b2f95457139ea2de
Guilherme Pangnotta <setetres@gmail.com>
2015-06-01 13:33:58 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -4,19 +4,28 @@
 $(document).ready(function() {
     'use strict';
 
+    // fullpage
+
     $('#fullpage').fullpage({
         css3: true,
         scrollingSpeed: 600,
         easingcss3: 'cubic-bezier(0.19, 1, 0.22, 1)',
-        anchors: ['hello', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'puta', 'cacete', 'founders', 'inkonik'],
+        anchors: ['hello', 'inkstarter', 'placevomit', 'ijwgh', 'xxxxxxxooo', 'cacete', 'founders'],
         navigation: true,
-        navigationTooltips: ['Hello', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'PUTA.nu', 'Cacete.co', 'Founders.com.br', 'Inkonik.com'],
+        navigationTooltips: ['Hello', 'InkStarter.cc', 'Placevom.it', 'IJWGH.CLUB', 'XXXXXXX.OOO', 'Cacete.co', 'Founders.com.br'],
         onSlideLeave: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].stop();
         },
         afterLoad: function(anchorLink){
             $('#' + anchorLink + '-wrapper').find('video')[0].play();
         }
+    });
+
+    // hamburger
+
+    $('.hamburger').on('click',function(){
+        $('#fp-nav').toggleClass('active');
+        return false;
     });
 
     // load
